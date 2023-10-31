415e258ab14c451fd61341ecfc8843c097fe5120
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-10-31 17:07:10 +0200

    update01

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,6 +4,7 @@ import Snake from "./components/Snake/Snake";
 
 function App() {
   const BOARD_LENGTH = 10;
+  const [direction, setDirection] = useState("right");
   const [snake, setSnake] = useState([
     { x: 3, y: 0 },
     { x: 2, y: 0 },
@@ -11,30 +12,59 @@ function App() {
     { x: 0, y: 0 },
   ]);
 
-  const pressKeyHangler = useCallback(
-    (e: KeyboardEvent) => {
-      console.log(e.key);
-      const newSnake = [...snake];
-      const snakeHead = { ...newSnake[0] };
+  const pressKeyHangler = useCallback((e: KeyboardEvent) => {
+    switch (e.key) {
+      case "ArrowRight":
+        setDirection("right");
+        break;
+      case "ArrowLeft":
+        setDirection("left");
+        break;
+      case "ArrowUp":
+        setDirection("up");
+        break;
+      case "ArrowDown":
+        setDirection("down");
+        break;
+      default:
+        break;
+    }
+  }, []);
 
-      if (e.key === "ArrowRight") {
-        snakeHead.x += 1;
-      } else if (e.key === "ArrowLeft") {
-        snakeHead.x -= 1;
-      } else if (e.key === "ArrowUp") {
-        snakeHead.y -= 1;
-      } else if (e.key === "ArrowDown") {
-        snakeHead.y += 1;
-      }
+  const snakeMoveHandler = useCallback(() => {
+    const newSnake = [...snake];
+    const snakeHead = { ...newSnake[0] };
 
-      newSnake.unshift(snakeHead);
-      if (newSnake.length > 1) {
-        newSnake.pop();
-      }
-      setSnake(newSnake);
-    },
-    [snake]
-  );
+    switch (direction) {
+      case "right":
+        snakeHead.x + 1 > 9 ? (snakeHead.x = 0) : (snakeHead.x += 1);
+        break;
+      case "left":
+        snakeHead.x - 1 < 0 ? (snakeHead.x = 9) : (snakeHead.x -= 1);
+        break;
+      case "up":
+        snakeHead.y - 1 < 0 ? (snakeHead.y = 9) : (snakeHead.y -= 1);
+        break;
+      case "down":
+        snakeHead.y + 1 > 9 ? (snakeHead.y = 0) : (snakeHead.y += 1);
+        break;
+      default:
+        break;
+    }
+
+    newSnake.unshift(snakeHead);
+    if (newSnake.length > 1) {
+      newSnake.pop();
+    }
+    setSnake(newSnake);
+  }, [snake, direction]);
+
+  useEffect(() => {
+    const moveInterval = setInterval(snakeMoveHandler, 200);
+    return () => {
+      clearInterval(moveInterval);
+    };
+  }, [snakeMoveHandler]);
 
   useEffect(() => {
     document.addEventListener("keydown", pressKeyHangler);
